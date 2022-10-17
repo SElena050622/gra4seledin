b4c79cefb6c346edc4df99f9db4b5629aea88b51
Сем Елена <semova.s81@bk.ru>
2022-10-17 07:30:10 +0300

    for lector 2

--- src/test/java/KardTest.java
+++ src/test/java/KardTest.java
@@ -1,7 +1,6 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
+import org.jsoup.select.Selector;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,10 +8,9 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class KardTest {
 
@@ -20,20 +18,30 @@ public class KardTest {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
+    @AfterEach
+    void tearDown() {
+        clearBrowserCookies();
+        closeWebDriver();
+    }
     @Test
     void kardDeliveryOrder() {
-        String date = genratDate(6);
         Selenide.open("http://localhost:9999");
-        ElementsCollection items = $$(".tab-item");
-        items.find(Condition.exactText("Встреча успешно забронирована на 17.10.2022")).click();
-        //$("[name = 'city']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[name = 'city']").setValue("Казань");
-        $("[name = 'date']").setValue("2022-10-17");
-        $("[name = 'name']").setValue("Ив Иван");
-        $("[name = 'phone']").setValue("+79998888888");
-        SelenideElement blok = $("[data-test-id = 'agreement']");
-        blok.$("[name = 'checkbox']").setValue("checkbox_icon");
-        $$("button").find(Condition.exactText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        String date = genratDate(6);
+        $("[data-test-id = 'city'] input").setValue("Казань");
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id = 'date'] input").setValue(date);
+        $("[data-test-id = 'name'] input").setValue("Ив Иван");
+        $("[data-test-id = 'phone'] input").setValue("+79998888888");
+        $("[data-test-id = 'agreement']").click();
+        $$("button").find(text("Забронировать")).click();
+        //$(Selectors.byText("Успешно!" + "Встреча успешно забронирована на " + date)).shouldBe(visible, Duration.ofSeconds(15));
+        //$(Selectors.byText("Встреча успешно забронирована на " + date)).shouldBe(visible, Duration.ofSeconds(15));
+        $("[name = 'notification__title']").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(text("Успешно!"));
+        //$("[name = 'notification__content']").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(text("Встреча успешно забронирована на " + date));
+        //$("[data-test-id = 'notification'] .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(text("Успешно!"));
+        //SelenideElement blok = $("[data-test-id = 'notification']");
+        //blok.$("[name = 'notification__title']").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(text("Успешно!"));
+        //blok.$("[name = 'notification__content']").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(text("Встреча успешно забронирована на " + date));
+        //$(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
     }
 }
